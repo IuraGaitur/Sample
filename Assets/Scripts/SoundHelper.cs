7d1a3e5e2b6b86b270e76b535a592691fb37ed83
Iura Gaitur <iura.gaitur@gmail.com>
2014-05-10 12:46:18 +0400

    Files update

---
+++ Assets/Scripts/SoundHelper.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundHelper : MonoBehaviour {
+
+	public static SoundHelper Instance;
+
+	public AudioClip _startSound;
+	public AudioClip _gameSound;
+	public AudioClip _holeInSound;
+	public AudioClip _mainInUrmaVaRog;
+	public AudioClip _shiniNuOTransmis;
+	public AudioClip _dvijokMashina;
+
+
+	void Awake()
+	{
+		// Register the singleton
+		if (Instance != null)
+		{
+			Debug.LogError("Multiple instances of SoundEffectsHelper!");
+		}
+		Instance = this;
+	}
+	
+	public void startSound()
+	{
+		MakeSound(_startSound);
+	}
+	
+	public void gameSound()
+	{
+		MakeSound(_gameSound);
+	}
+
+	public void holeInSound()
+	{
+		MakeSound(_holeInSound);
+	}
+	
+	public void mainInUrmaVaRog()
+	{
+		MakeSound(_mainInUrmaVaRog);
+	}
+
+	public void shiniNuOTransmis()
+	{
+		MakeSound(_shiniNuOTransmis);
+	}
+	
+	public void dvijokMashina()
+	{
+		MakeSound(_dvijokMashina);
+	}
+	
+
+	
+	/// <summary>
+	/// Play a given sound
+	/// </summary>
+	/// <param name="originalClip"></param>
+	private void MakeSound(AudioClip originalClip)
+	{
+		// As it is not 3D audio clip, position doesn't matter.
+		AudioSource.PlayClipAtPoint(originalClip, transform.position);
+	}
+}
